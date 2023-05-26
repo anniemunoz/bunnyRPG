91b28e8a93cbc739874ddcfc076c836da3ee4312
anniemunoz <anipaola95@gmail.com>
2023-05-25 19:56:22 -0500

    Add first script of movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 4.0f;
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { // s= v*t
+        var h = Input.GetAxisRaw(horizontal);
+        var v = Input.GetAxisRaw(vertical);
+
+        if (Mathf.Abs(h) > 0.5f)
+        {
+            this.transform.Translate(new Vector3(h * speed * Time.deltaTime, 0, 0));
+        }
+        if (Mathf.Abs(v) > 0.5f)
+        {
+            this.transform.Translate(new Vector3(0, v * speed * Time.deltaTime, 0));
+        }
+    }
+}
