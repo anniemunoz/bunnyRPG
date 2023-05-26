bc5023129cbbff4b9d33d7bef87a6bd39e753cd3
anniemunoz <anipaola95@gmail.com>
2023-05-26 07:56:44 -0500

    Change to blender tree and update script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 4.0f;
+    private bool walking = false;
+    public Vector2 lastMovement = Vector2.zero;
+
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
     private Animator animator;
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+    private const string walkingState = "Walking";
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +25,25 @@ public class PlayerController : MonoBehaviour
     { // s= v*t
         var h = Input.GetAxisRaw(horizontal);
         var v = Input.GetAxisRaw(vertical);
+        walking = false;
 
         if (Mathf.Abs(h) > 0.5f)
         {
             this.transform.Translate(new Vector3(h * speed * Time.deltaTime, 0, 0));
+            walking = true;
+            lastMovement = new Vector2(h, 0);
         }
         if (Mathf.Abs(v) > 0.5f)
         {
             this.transform.Translate(new Vector3(0, v * speed * Time.deltaTime, 0));
+            walking = true;
+            lastMovement = new Vector2(0, v);
         }
 
         animator.SetFloat(horizontal, h);
         animator.SetFloat(vertical, v);
+        animator.SetBool(walkingState, walking);
+        animator.SetFloat(lastHorizontal, lastMovement.x);
+        animator.SetFloat(lastVertical, lastMovement.y);
     }
 }
